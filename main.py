a7722ba8ae7a804f11d4d841c40a3c9f063e38d3
Dmitriy Kolmakov <kolmakov125@gmail.com>
2024-06-02 15:22:33 +0300

    add cli to main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import argparse
 import logging
-import sys
 from collections import deque
 
 from isa import read_code
@@ -7,37 +7,52 @@ import control_unit as cu
 import data_path as dp
 
 
-def main(code_file_name: str, input_file_name: str) -> None:
-    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+def main(code_file: str, input_file: str | None, limit: int, char_output: bool, debug_logging: bool) -> None:
+    logging.getLogger().setLevel(logging.DEBUG if debug_logging else logging.INFO)
 
-    code: list = read_code(code_file_name)
+    code = read_code(code_file)
 
-    with open(input_file_name, encoding="utf-8") as file:
-        input_text: str = file.read()
-    input_tokens: deque = deque(map(ord, input_text))
-    input_tokens.appendleft(len(input_tokens))
+    if input_file is None:
+        input_tokens: deque = deque()
+    else:
+        with open(input_file, encoding="utf-8") as file:
+            input_text: str = file.read()
+        input_tokens: deque = deque(map(ord, input_text))
+        input_tokens.appendleft(len(input_tokens))
 
     io_unit = dp.IOUnit(input_buffer=input_tokens)
     io_controller = dp.IOController()
     io_controller.connect(port=1, unit=io_unit)
 
     data_path: dp.DataPath = dp.DataPath(
-        memory_size=1024,
         program=code,
         io_controller=io_controller)
     control_unit: cu.ControlUnit = cu.ControlUnit(data_path=data_path)
     data_path.control_unit = control_unit
 
-    ticks = control_unit.run_simulation()
+    ticks = control_unit.run_simulation(tick_limit=limit)
 
     io_controller.disconnect(port=1)
 
-    print(f"Output: {io_unit.get_list_output()}")
+    if char_output:
+        print(f"Output: {io_unit.get_str_output()}")
+    else:
+        print(f"Output: {io_unit.get_list_output()}")
     print(f"Ticks: {ticks}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        raise Exception("Wrong arguments: machine.py <code_file> <input_file>")
-    _, code_file, input_file = sys.argv
-    main(code_file, input_file)
+    parser = argparse.ArgumentParser(description="Stack processor simulation")
+    parser.add_argument("code_file", help="Binary code file name")
+    parser.add_argument("input_file", nargs="?", help="Input file name (optional)")
+    parser.add_argument("-l", "--limit", type=int, default=200000, help="Tick limit (default 200000)")
+    parser.add_argument("-c", "--char", action='store_true', help="Char output (default - list[int])")
+    parser.add_argument("-d", "--debug", action='store_true', help="Debug logging (default - info)")
+
+    args = parser.parse_args()
+
+    try:
+        main(args.code_file, args.input_file, args.limit, args.char, args.debug)
+    except Exception as e:
+        logging.error(e)
+        raise e
