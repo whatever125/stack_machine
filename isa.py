4b4ff526a55ef7ff5563224cab26ecc2fbac6cc5
Dmitriy Kolmakov <kolmakov125@gmail.com>
2024-06-02 15:23:49 +0300

    change isa opcodes, add size and bits

--- isa.py
+++ isa.py
@@ -2,58 +2,57 @@ import struct
 from enum import IntEnum, auto
 
 
+MEMORY_SIZE = 2048
+BITS = 32
+MIN_SIGN = -(2 ** (BITS - 1))
+MAX_SIGN = 2 ** (BITS - 1) - 1
+
+
 class Opcode(IntEnum):
     # no op
-    NOP = auto()
+    NOP = 0x00000
 
     # stack ops
-    PUSH = auto()
-    POP = auto()
-    SWAP = auto()
-    DUP = auto()
-    OVER = auto()
+    PUSH = 0x00001
+    POP = 0x00010
+    SWAP = 0x00011
+    DUP = 0x00100
+    OVER = 0x00101
 
-    # math
-    INC = auto()
-    DEC = auto()
-    ADD = auto()
-    SUB = auto()
-    MUL = auto()
-    DIV = auto()
-    MOD = auto()
+    # math ops
+    INC = 0x00110
+    DEC = 0x00111
+    ADD = 0x01000
+    SUB = 0x01001
+    MUL = 0x01010
+    DIV = 0x01011
+    MOD = 0x01100
 
-    # memory
-    LOAD = auto()
-    SAVE = auto()
+    # memory ops
+    LOAD = 0x01101
+    SAVE = 0x01110
 
-    # io
-    IN = auto()
-    OUT = auto()
+    # io ops
+    IN = 0x01111
+    OUT = 0x10000
 
-    # jump
-    JMP = auto()
-    JZ = auto()
-
-    # functions
-    CALL = auto()
-    RET = auto()
-
-    # stop
-    HALT = auto()
-
-    def __str__(self):
-        return str(self.value)
+    # flow ops
+    JMP = 0x10001
+    JZ = 0x10010
+    CALL = 0x10011
+    RET = 0x10100
+    HALT = 0x11111
 
 
-def get_opcode_names():
+def get_opcode_names() -> list[str]:
     return [e.name for e in Opcode]
 
 
-def read_code(filename: str) -> list:
+def read_code(filename: str) -> list[Opcode | int]:
     with open(filename, "rb") as file:
         data = file.read(4)
         index = 0
-        start = struct.unpack("i", data)[0]
+        start: int = struct.unpack("i", data)[0]
         code = [start]
         data = file.read(4)
         while data:
@@ -64,23 +63,26 @@ def read_code(filename: str) -> list:
                     and isinstance(code[-1], Opcode)
                     and code[-1] in [Opcode.PUSH]
                 ):
-                    value = struct.unpack("i", data)[0]
+                    value: int = struct.unpack("i", data)[0]
                     code.append(value)
                 else:
-                    value = struct.unpack("i", data)[0]
+                    value: int = struct.unpack("i", data)[0]
                     opcode = Opcode(value)
                     code.append(opcode)
             else:
-                value = struct.unpack("i", data)[0]
+                value: int = struct.unpack("i", data)[0]
                 code.append(value)
             data = file.read(4)
     return code
 
 
-def write_code(target_name: str, code: list[int], commented_code: str):
+def write_code(target_name: str, code: list[int]) -> None:
     with open(target_name, "wb") as file:
         file.write(struct.pack(f"{len(code)}i", *code))
+
+
+def write_commented_code(target_name: str, code: str) -> None:
     with open(
         target_name[: target_name.find(".")] + "_com.txt", "w", encoding="utf-8"
     ) as file:
-        file.write(commented_code)
+        file.write(code)
